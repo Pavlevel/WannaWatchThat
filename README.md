436c6828b5705ee7b7989fcce26dd457b4f6e4c0
Pavle <pavlevel@gmail.com>
2023-09-21 18:30:28 +0200

    second push

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 WannaWatchThat?
 
-Baza 
+Baza filmova i serija sa ciljem da korisniku prikaze najnovije filmove i serije i omoguci mu da napravi listu serija i filmova koje bi gledao.
 
 Tehnologije:
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,70 @@
-import { Heading } from "@chakra-ui/react"
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Icon,
+  Text,
+} from "@chakra-ui/react";
+import { VscAccount } from "@react-icons/all-files/vsc/VscAccount.esm";
 
 const Home = () => {
+  const handleLogin = () => {};
+
   return (
     <div>
-      <Heading>Home Page</Heading>
-    </div>
-  )
-}
+      <Flex
+        flexDir={"row"}
+        height={"16"}
+        alignItems={"center"}
+        justify={"center"}
+        bg={"#008E89"}
+        width={"full"}
+      >
+        <Heading bg={"#008E89"}>WWT</Heading>
 
-export default Home
+        <Button
+          _hover={{ bg: "#FFD32D", color: "white" }}
+          bg={"#008E89"}
+          onClick={handleLogin}
+          w={10}
+          h={"fit-content"}
+        >
+          <Icon
+            _hover={{ bg: "#FFD32D", color: "black", borderRadius: 6 }}
+            as={VscAccount}
+            bg={"#008E89"}
+            w={10}
+            h={10}
+            color="red.500"
+          />
+        </Button>
+        <Button bg={"#008E89"} onClick={handleLogin} w={8} h={"fit-content"}>
+          <Icon as={VscAccount} bg={"#008E89"} w={8} h={8} color="red.500" />
+        </Button>
+      </Flex>
+
+      <Container maxW={"container.md"} centerContent>
+        <Flex flexDir={"column"} gap={"4"}>
+          <Text>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid,
+            ad. Autem sit provident debitis molestiae quas voluptatem esse
+            deleniti. Eos nostrum similique suscipit harum temporibus mollitia
+            inventore cupiditate! Consectetur, debitis.
+          </Text>
+
+          <Button
+            bg={"#008E89"}
+            color={"#fff"}
+            _hover={{ bg: "#FFD32D", color: "#000" }}
+          >
+            Click
+          </Button>
+        </Flex>
+      </Container>
+    </div>
+  );
+};
+
+export default Home;
